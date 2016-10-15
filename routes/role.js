0dbadd7538a1641f4bc27e22b14c0d2d84fabe5f
Kecc16 <kevinjoan16@gmail.com>
2016-10-14 23:52:00 -0500

    Se crea la ruta POST /roles para crear un rol.

--- routes/role.js
+++ routes/role.js
@@ -13,5 +13,19 @@ module.exports = [
                 reply(roles);
             });
         }
+    },
+    {
+        method: 'POST',
+        path: '/roles',
+        handler: function(request, reply) {
+            var role = request.payload;
+            roleController.create(role, function(err, res) {
+                if(err){
+                    console.log(err);
+                    reply(err);
+                }
+                reply(res);
+            });
+        }
     }
 ]
