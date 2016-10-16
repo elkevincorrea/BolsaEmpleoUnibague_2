2d92e040792f4d0d7fa2831aad261bcf08e16374
Kecc16 <kevinjoan16@gmail.com>
2016-10-16 12:19:08 -0500

    Se crea el controlador para el usuario con funciones: -Obtener todos los usuarios -Crear un usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var models = require('../models');
+
+var UserController = {
+    getAll: function(callback) {
+        models.User.findAll().then(function(users) {
+            callback(null, users);
+        }, function(err) {
+            callback(err, null);
+        });
+    },
+    create: function(user, callback) {
+        models.User.create(user).then(function(res) {
+            callback(null, res);
+        }, function(err) {
+            callback(err, null);
+        });
+    }
+}
