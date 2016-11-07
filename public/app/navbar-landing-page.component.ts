507d6f1d57d290470ea62eacc9ec6e568c28f509
DESKTOP-JK6QKC1\Daniel Perez <daniperez2014@outlook.es>
2016-11-07 15:44:53 -0500

    feat: Se creo el componente para la navbar de la landing page

---
+++ public/app/navbar-landing-page.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'navbar-landing-page',
+    templateUrl: '/app/templates/navbar-landing-page.component.html'
+
+})
+
+export class NavbarLandingPageComponent{}
