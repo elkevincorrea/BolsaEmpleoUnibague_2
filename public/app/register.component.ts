37dc731f65c6667fd6413075cc877b6695ad9394
DESKTOP-JK6QKC1\Daniel Perez <daniperez2014@outlook.es>
2016-11-07 15:43:52 -0500

    feat: Se creo component para el formulario de registro

---
+++ public/app/register.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'register-landing',
+    templateUrl: '/app/templates/register.component.html'
+})
+export class RegisterLandingComponent{}
