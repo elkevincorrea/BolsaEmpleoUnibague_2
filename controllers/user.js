c19d7e6953f8fb0bf37f3502e1ce2f59a2279ab8
Kecc16 <kevinjoan16@gmail.com>
2016-11-07 21:59:48 -0500

    feat: Se encripta contraseña crear usuario, se busca un usuario tipo empresa.

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const passwordCrypt = require('../util/password-crypt.js');
+
 var models = require('../models');
 
 var UserController = {
@@ -13,12 +15,42 @@ var UserController = {
         });
     },
     create: function(user, callback) {
-        models.User.create(user, {
-            include: [models.Person, models.Company]
-        }).then(function(res) {
-            callback(null, res);
-        }, function(err) {
-            callback(err, null);
+        passwordCrypt.cryptPassword(user.password, function(err, hash) {
+            if(err){
+                callback(err, null);
+            }else{
+                user.password = hash;
+                models.User.create(user, {
+                    include: [models.Person, models.Company]
+                }).then(function(res) {
+                    callback(null, res);
+                }, function(err) {
+                    callback(err, null);
+                });
+            }
+        });
+    },
+    getCompanyById: function(user, callback) {
+        models.User.findOne({
+            where: {
+                company_identification: user.identification
+            },
+            include: [models.Company]
+        }).then(function(resUser) {
+            passwordCrypt.comparePassword(user.password, resUser.password, function(err, res) {
+                if(err){
+                    //Contraseña Incorrecta
+                    callback(err, null);
+                }else{
+                    if(res == true){
+                        callback(null, resUser);
+                    }else{
+                        callback({
+                            message: 'Contraseña incorrecta'
+                        }, null);
+                    }
+                }
+            })
         });
     }
 }
