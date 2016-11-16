8e0ed79a9b4fb9404c68c747b84ce4f560a31f0d
Kecc16 <kevinjoan16@gmail.com>
2016-11-15 21:14:34 -0500

    feat: Se añaden las relaciones n:m al modelo de perfil profesional.

--- models/professional_profile.js
+++ models/professional_profile.js
@@ -13,7 +13,18 @@ module.exports = function(sequelize, DataTypes) {
       underscored: true,
       timestamps: false,
       associate: function(models) {
-        
+        Professional_Profile.belongsToMany(models.Degree, {
+          as: 'Degrees',
+          through: models.Pro_Prof_Degrees
+        });
+        Professional_Profile.belongsToMany(models.Experience, {
+          as: 'Experiences',
+          through: models.Pro_Prof_Exps
+        });
+        Professional_Profile.belongsToMany(models.Language, {
+          as: 'Languages',
+          through: models.Pro_Prof_Langs
+        });
       }
     }
   });
