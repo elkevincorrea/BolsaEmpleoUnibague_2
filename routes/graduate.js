6903e3a3a0c0e264e084a5f7a31bb58a222a1d24
Kecc16 <kevinjoan16@gmail.com>
2016-11-20 20:34:51 -0500

    feat: POST '/bolsa-empleo/graduates/postulations' ruta para postularse a una vacante.

--- routes/graduate.js
+++ routes/graduate.js
@@ -49,5 +49,24 @@ module.exports = [
                 }
             });
         }
+    },
+    {
+        method: 'POST',
+        path: '/bolsa-empleo/graduates/postulations',
+        handler: function(request, reply) {
+            if(!request.auth.credentials.Graduate || !request.auth.credentials.Graduate.identification){
+                return reply({"status": 401,"message": "Unauthorized"});
+            }
+            var id_graduate = request.auth.credentials.Graduate.identification;
+            var id_vacancy = request.payload.id_vacancy;
+            GraduateController.postulateToVacancy(id_graduate, id_vacancy, function(err, res) {
+                if(err){
+                    console.log(err);
+                    reply(err);
+                }else{
+                    reply(res);
+                }
+            });
+        }
     }
 ];
