f2c17668ec5b8a7c6c7a2a91b07ccf936e739df5
DESKTOP-JK6QKC1\Daniel Perez <daniperez2014@outlook.es>
2016-11-20 20:56:41 -0500

    feat: se creo el componente para la navbar-default

---
+++ public/app/shared/navbar-default.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    
+    selector: 'navbar-default',
+    templateUrl: '/app/templates/navbar-default.html',
+    styleUrls : ['../css/navbar-default.css']  
+})
+export class NavbarDefaultComponent {
+  
+}
