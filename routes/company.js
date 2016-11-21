f64a42e057a4f41f5df966619affb02495b50e34
Kecc16 <kevinjoan16@gmail.com>
2016-11-21 13:35:02 -0500

    feat: Se añaden rutas a la compañia: -PATCH /bolsa-empleo/companies/{id} actualiza una empresa -GET /bolsa-empleo/companies/{id_vacancy}/postulations obtiene los postulado para la vacante de una empresa -PATCH /bolsa-empleo/companies/{id_vacancy}/postulations/{id_person} cambia el estado de postulacion dado el id de la vacante y la identificacion del graduado.

--- routes/company.js
+++ routes/company.js
@@ -34,6 +34,22 @@ module.exports = [
         }
     },
     {
+        method: 'PATCH',
+        path: '/bolsa-empleo/companies/{id}',
+        handler: function(request, reply){
+            var company_id = request.params.id;
+            var company = request.payload;
+            CompanyController.update(company_id, company, function(err, res) {
+                if(err){
+                    console.log(err);
+                    reply(err);
+                }else{
+                    reply(res);
+                }
+            });
+        }
+    },
+    {
         method: 'GET',
         path: '/bolsa-empleo/companies/vacancies',
         handler: function(request, reply){
@@ -68,6 +84,46 @@ module.exports = [
                     reply(res);
                 }
             })
+        }
+    },
+    {
+        method: 'GET',
+        path: '/bolsa-empleo/companies/vacancies/{id_vacancy}/postulations',
+        handler: function(request, reply){
+            if(!request.auth.credentials.Company || !request.auth.credentials.Company.identification){
+                return reply({status: 401, message:'Unathorized'});
+            }
+            var company_id = request.auth.credentials.Company.identification;
+            var vacancy_id = request.params.id_vacancy
+            CompanyController.getPostulatesVacancy(company_id, vacancy_id, function(err, res) {
+                if(err){
+                    console.log(err);
+                    reply(err);
+                }else{
+                    reply(res);
+                }
+            });
+        }
+    },
+    {
+        method: 'PATCH',
+        path: '/bolsa-empleo/companies/vacancies/{id_vacancy}/postulations/{id_person}',
+        handler: function(request, reply){
+            if(!request.auth.credentials.Company || !request.auth.credentials.Company.identification){
+                return reply({status: 401, message:'Unathorized'});
+            }
+            var company_id = request.auth.credentials.Company.identification;
+            var vacancy_id = request.params.id_vacancy;
+            var person_id = request.params.id_person;
+            var status = request.payload.status;
+            CompanyController.updatePostulation(company_id, vacancy_id, person_id, status, function(err, res) {
+                if(err){
+                    console.log(err);
+                    reply(err);
+                }else{
+                    reply(res);
+                }
+            });
         }
     }
 ];
