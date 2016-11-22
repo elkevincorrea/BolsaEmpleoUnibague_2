a10d3e704039627939e542e02addb81255cdc4b2
Kecc16 <kevinjoan16@gmail.com>
2016-11-21 20:18:03 -0500

    feat: Se configura el controlador para: -Utilizar el servicio User -Registrar un graduado

--- public/app/landing/register.component.ts
+++ public/app/landing/register.component.ts
@@ -1,7 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Person, Company, User} from './../models/models';
+import { UserService } from './../services/user.service'
 
 @Component({
     selector: 'register-landing',
-    templateUrl: '/app/templates/register.component.html'
+    templateUrl: '/app/templates/register.component.html',
+    providers: [UserService]
 })
-export class RegisterLandingComponent{}
+export class RegisterLandingComponent implements OnInit{
+
+    errorMessage: string;
+    succesMessage: string;
+    user: User = new User();
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit() {
+        this.user.Company = new Company();  
+     }
+
+    switch(option: string) {
+        this.user.type = option;
+    };
+
+    submitGraduate(){
+        if(this.user.type=='graduate'){
+            this.user.Company = null;
+        }
+        console.log(this.user.identification);
+        this.userService.registerGraduateUser(this.user)
+                        .subscribe(
+                            user => { 
+                                this.user = user;
+                                this.succesMessage = 'Ya puedes ingresar';
+                            },
+                            error => this.errorMessage = error
+                        );
+    }
+
+    get diagnostic() {
+        return JSON.stringify(this.user);
+    }
+}
