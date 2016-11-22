a9196ce990ff940da9610190263c565d88a2b2b7
Kecc16 <kevinjoan16@gmail.com>
2016-11-22 14:29:01 -0500

    feat: Se configura y se crea función para logearse.

--- public/app/landing/navbar-landing-page.component.ts
+++ public/app/landing/navbar-landing-page.component.ts
@@ -1,9 +1,54 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './../services/authentication.service'
+import { User } from './../models/models'
 
 @Component({
     selector: 'navbar-landing-page',
     templateUrl: '/app/templates/navbar-landing-page.component.html'
-
 })
+export class NavbarLandingPageComponent implements OnInit{
+    user: User = new User();
+    loading = false;
+    error = '';
+    
+    constructor(private authService: AuthenticationService){ }
 
-export class NavbarLandingPageComponent{}
+    ngOnInit() {
+        this.user.type = 'graduate';
+        var type = this.authService.logged();
+        if(type == 'company'){
+            window.location.href = '/empresa'
+        }else if(type == 'graduate'){
+            window.location.href = '/graduado';
+        }else if(type == 'admin'){
+            window.location.href = '/admin';
+        }
+     }
+
+     login() {
+        this.loading = true;
+        if(this.user.type == 'graduate'){
+            this.authService.loginGraduate(this.user.identification, this.user.password)
+                .subscribe(result => {
+                    if(result === true){
+                        console.log('Graduado ok');
+                        window.location.href = '/graduado';
+                    }else{
+                        this.error = 'Usuario o contraseña incorrecta';
+                        this.loading = false;
+                    }
+                });
+        }else if(this.user.type == 'company'){
+            this.authService.loginCompany(this.user.identification, this.user.password)
+                .subscribe(result => {
+                    if(result === true){
+                        console.log('Compañia ok')
+                        window.location.href = '/empresa'
+                    }else{
+                        this.error = 'Usuario o contraseña incorrecta';
+                        this.loading = false;
+                    }
+                });
+        }
+    }
+}
