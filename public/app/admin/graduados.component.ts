101b7aa8e3b01a7bcdd24ed26f9c371bd916d7bb
Daniel Perez <daniel2213perez@gmail.com>
2016-11-21 19:47:47 -0500

    feat: Se añaden campos a graduado en el controlador y en el template

--- public/app/admin/graduados.component.ts
+++ public/app/admin/graduados.component.ts
@@ -4,11 +4,14 @@ export class Graduado {
     identification: number;
     first_name: string;
     last_name: string;
+    telefono : number;
+    email: string;
+    genero: string;
 }
 
 const GRADUADOS: Graduado[] = [
-    {identification: 1000000, first_name: 'John', last_name: 'Doe'},
-    {identification: 1000001, first_name: 'Daniel', last_name: 'Perez'}
+    {identification: 1000000, first_name: 'John', last_name: 'Doe', telefono:31245, email:'doe@gmail.com',genero:'F'},
+    {identification: 1000001, first_name: 'Daniel', last_name: 'Perez', telefono: 123156, email:'doe@gmail.com',genero:'M'}
 ]
 
 @Component({
