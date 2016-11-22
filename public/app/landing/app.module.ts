7cbc153da71211ab6c07db2b3ad129770c910485
Kecc16 <kevinjoan16@gmail.com>
2016-11-22 14:25:25 -0500

    feat: Se agrega el servicio de autenticación al modulo principal de la landing

--- public/app/landing/app.module.ts
+++ public/app/landing/app.module.ts
@@ -6,11 +6,15 @@ import { DescriptionComponent} from './description.component';
 import { NavbarLandingPageComponent} from './navbar-landing-page.component';
 import{RegisterLandingComponent} from './register.component';
 import { HttpModule, JsonpModule } from '@angular/http';
+import {AuthenticationService} from './../services/authentication.service'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule, JsonpModule ],
   declarations: [ 
     AppComponent, DescriptionComponent , NavbarLandingPageComponent, RegisterLandingComponent
+  ],
+  providers: [
+    AuthenticationService
   ],
   bootstrap:    [ AppComponent ]
 })
