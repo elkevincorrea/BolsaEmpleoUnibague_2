6d3bbae48fad593abd64102564510a63494bc1ec
Kecc16 <kevinjoan16@gmail.com>
2016-11-22 00:38:16 -0500

    feat: Se añade modelo IdentificationType

--- public/app/models/models.ts
+++ public/app/models/models.ts
@@ -1,3 +1,8 @@
+export class IdentificationType{
+    public id: number;
+    public name: string;
+}
+
 export class Person {
     public identification: number;
     public first_name: string;
@@ -6,6 +11,7 @@ export class Person {
 
 export class Company {
     public identification: number;
+    public identification__type_id: number;
     public name: string;
     public website: string;
     public phone: number;
