f680339c068fd0c94657158e5f9c6b0c06e77cc3
Kecc16 <kevinjoan16@gmail.com>
2016-11-22 14:23:20 -0500

    feat: Se crea servicio de autenticación con funciones: -Ingresar como graduado -Ingresar como empresa -Logout -Logged función que indica como que usuario (admin, empresa, graduado) esta logeado

---
+++ public/app/services/authentication.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map'
+ 
+@Injectable()
+export class AuthenticationService {
+    public token: string;
+ 
+    constructor(private http: Http) {
+        // set token if saved in local storage
+        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        this.token = currentUser && currentUser.token;
+    }
+ 
+    loginGraduate(identification: number, password: string): Observable<boolean> {
+        return this.http.post('/bolsa-empleo/auth/users/graduate', { identification: identification, password: password })
+            .map((response: Response) => {
+                // login successful if there's a jwt token in the response
+                console.log(response.json());
+                let token = response.json() && response.json().token;
+                console.log(token);
+                if (token) {
+                    // set token property
+                    this.token = token;
+ 
+                    // store username and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify({ identification: identification, user: 'graduate', token: token }));
+ 
+                    // return true to indicate successful login
+                    return true;
+                } else {
+                    // return false to indicate failed login
+                    return false;
+                }
+            });
+    }
+
+    loginCompany(identification: number, password: string): Observable<boolean> {
+        return this.http.post('/bolsa-empleo/auth/users/company', JSON.stringify({ identification: identification, password: password }))
+            .map((response: Response) => {
+                // login successful if there's a jwt token in the response
+                let token = response.json() && response.json().token;
+                if (token) {
+                    // set token property
+                    this.token = token;
+ 
+                    // store username and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify({ identification: identification, user: 'company', token: token }));
+ 
+                    // return true to indicate successful login
+                    return true;
+                } else {
+                    // return false to indicate failed login
+                    return false;
+                }
+            });
+    }
+
+    logged(): string {
+        var currentUser = JSON.parse(localStorage.getItem('currentUser')); 
+        if (!currentUser) {
+            return null;
+        }else{
+            return currentUser.user; 
+        }
+        
+    }
+ 
+    logout(): void {
+        // clear token remove user from local storage to log user out
+        this.token = null;
+        localStorage.removeItem('currentUser');
+    }
+}
