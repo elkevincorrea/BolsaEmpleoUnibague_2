653481c80ae7b50ade0999868daca206fc9563a2
Kecc16 <kevinjoan16@gmail.com>
2016-11-22 00:39:54 -0500

    feat: Se configura componente para poder inscribirse como empresa

--- public/app/landing/register.component.ts
+++ public/app/landing/register.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import {Person, Company, User} from './../models/models';
-import { UserService } from './../services/user.service'
+import {Person, Company, User, IdentificationType} from './../models/models';
+import { UserService } from './../services/user.service';
+import { IdentificationTypeService } from './../services/id_types.service';
 
 @Component({
     selector: 'register-landing',
     templateUrl: '/app/templates/register.component.html',
-    providers: [UserService]
+    providers: [UserService, IdentificationTypeService]
 })
 export class RegisterLandingComponent implements OnInit{
 
     errorMessage: string;
     succesMessage: string;
     user: User = new User();
+    idtypes: IdentificationType[];
 
-    constructor(private userService: UserService) { }
+    constructor(private userService: UserService, private idTypesService: IdentificationTypeService) { }
 
     ngOnInit() {
-        this.user.Company = new Company();  
+        this.user.Company = new Company();
+        this.user.Company.identification__type_id= null;
+        this.getIdTypes();
      }
 
     switch(option: string) {
         this.user.type = option;
     };
+
+    getIdTypes() {
+        this.idTypesService.getIdentificationTypes().subscribe(
+            id_types => this.idtypes = id_types,
+            error => this.errorMessage = <any>error
+        );
+    }
 
     submitGraduate(){
         if(this.user.type=='graduate'){
@@ -29,6 +40,17 @@ export class RegisterLandingComponent implements OnInit{
         }
         console.log(this.user.identification);
         this.userService.registerGraduateUser(this.user)
+                        .subscribe(
+                            user => { 
+                                this.user = user;
+                                this.succesMessage = 'Ya puedes ingresar';
+                            },
+                            error => this.errorMessage = error
+                        );
+    }
+
+    submitCompany(){
+        this.userService.registerCompanyUser(this.user)
                         .subscribe(
                             user => { 
                                 this.user = user;
