1b19a355d21f2ac36dfa2f2616c304a05c0d53b7
Daniel Perez <daniel2213perez@gmail.com>
2016-11-21 19:49:08 -0500

    feat: Se crea el componente para la lista de vacantes del admin con una configuración inicial

---
+++ public/app/admin/vacantes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    
+    selector: 'vacantes-admin',
+    template: '<h2>Vacantes en proceso</h2>'
+})
+export class VacantesComponent  {
+    
+}
