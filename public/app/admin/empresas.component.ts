3872b9b3fc64acf0fee8b244ec9e9a952b7615b9
Daniel Perez <daniel2213perez@gmail.com>
2016-11-21 19:48:44 -0500

    feat: Se crean empresas como variable locar en el controlador, y se hace uso del template html

--- public/app/admin/empresas.component.ts
+++ public/app/admin/empresas.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Empresa{
+
+    identificacion: number;
+    tipo_identificacion: string;
+    nombre: string;
+    pagina_web: string;
+    telefono: number;
+    estado: boolean;
+}
+
+const EMPRESAS: Empresa[] = [
+    {identificacion: 13156, tipo_identificacion: 'nit', nombre:'KECC', pagina_web:'master.com', telefono: 16546, estado:true},
+    {identificacion: 13156, tipo_identificacion: 'rut', nombre:'otra', pagina_web:'otra.com', telefono: 11416546, estado:false}
+]
+
 @Component({
    
     selector: 'empresas-admin',
-    template: '<h2>probando companyssssss</h2>'
+    templateUrl: '/app/templates/tabla-empresas.component.html'
 })
 export class EmpresasComponent  {
-    
+    empresas = EMPRESAS;
 }
